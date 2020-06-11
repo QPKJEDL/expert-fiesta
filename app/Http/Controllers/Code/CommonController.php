1feacf678d73237c6a0274ad1a3d7e8183ad01b8
UltrmanOrbr <1216281190@qq.com>
2020-06-11 10:20:37 +0800

    hq api code

--- app/Http/Controllers/Code/CommonController.php
+++ app/Http/Controllers/Code/CommonController.php
@@ -11,11 +11,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
 use App\Models\Users;
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, Content-Type, Cookie, userid,token,X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN");
-header('Access-Control-Allow-Methods: POST,GET,PUT,DELETE,OPTIONS');
-header('Access-Control-Allow-Credentials', 'false');
-header('Access-Control-Expose-Headers', 'Authorization, authenticated');
 class CommonController extends Controller {
     protected $token = '';
     protected $uid='';
@@ -24,6 +19,11 @@ class CommonController extends Controller {
     public $imgurl="http://192.168.1.8";
     public $kefuurl="http://192.168.1.8";
     public function __construct() {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, Content-Type, Cookie, userid,token,X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN");
+        header('Access-Control-Allow-Methods: POST,GET,PUT,DELETE,OPTIONS');
+        header('Access-Control-Allow-Credentials', 'false');
+        header('Access-Control-Expose-Headers', 'Authorization, authenticated');
         $this->checkLogin();
     }
     //验证用户信息
