04325d76f0b665537efbbbe438ac2a754797e728
UltrmanOrbr <1216281190@qq.com>
2020-06-11 16:38:40 +0800

    hq api code

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -16,17 +16,20 @@ class EnableCrossRequestMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-        $allow_origin = [
-            'http://129.211.114.135:8900',
-        ];
-        if (in_array($origin, $allow_origin)) {
-            $response->header('Access-Control-Allow-Origin', $origin);
-            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, userid,token,X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
-            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-            $response->header('Access-Control-Allow-Credentials', 'true');
-        }
+//        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+//        $allow_origin = [
+//            'http://129.211.114.135:8900',
+//        ];
+//        if (in_array($origin, $allow_origin)) {
+//            $response->header('Access-Control-Allow-Origin', $origin);
+//            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, userid,token,X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+//            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
+//            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+//            $response->header('Access-Control-Allow-Credentials', 'true');
+//        }
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Headers', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         return $response;
 
     }
